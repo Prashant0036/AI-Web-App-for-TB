cb97306d92888f6df27ae6e26009a0b65317e44c
Prashant Saraswat <prashantsaraswat036@gmail.com>
2025-11-09 15:14:56 +0530

    Use Streamlit secrets for MongoDB URI

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 import pymongo
+import streamlit as st
 
-myclient = pymongo.MongoClient("URI String")
+myclient = pymongo.MongoClient(st.secrets["DB_URI"])
 
 mydb = myclient["db_TB_web_app"]
 # print(myclient.list_database_names())
