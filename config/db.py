2b049cb3a35560d74c68c408080e51bff6c04e20
Prashant0036 <prashantsaraswat036@gmail.com>
2025-11-09 16:28:38 +0530

    Revert last 5 commits

--- config/db.py
+++ config/db.py
@@ -1,15 +1,12 @@
-# config/db.py
-import os
-from dotenv import load_dotenv
 import pymongo
+import streamlit as st
 
-# Load .env locally; Streamlit Cloud will supply env via st.secrets and we set os.environ in frontend.
-load_dotenv()
+myclient = pymongo.MongoClient(st.secrets["DB_URI"])
 
-DB_URI = os.getenv("DB_URI")
-if not DB_URI:
-    raise RuntimeError("Missing DB_URI environment variable. Set DB_URI locally or in Streamlit secrets.")
-
-client = pymongo.MongoClient(DB_URI)
-mydb = client["db_TB_web_app"]
+mydb = myclient["db_TB_web_app"]
+# print(myclient.list_database_names())
 mycollection = mydb["Patient_Info"]
+# print(mydb.list_collection_names())
+
+# mydict = { "name": "John", "address": "Highway 37" }
+# x = mycollection.insert_one(mydict)
